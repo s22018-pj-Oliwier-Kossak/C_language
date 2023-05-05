3aac38255dcd45402101f8130586b3f43d00c550
Oliwier Kossak <s22018@pjwstk.edu.pl>
2023-05-05 18:36:18 +0200

    first use of scanf

---
+++ scanf/scanf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int b; 
+    int c;
+
+    scanf("%d\n", &a);
+    scanf("%d", &b);
+
+    c = a - 2 * b;
+
+    printf("%d",c);
+
+    return 0 ;
+}
