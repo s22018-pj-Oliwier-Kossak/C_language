ca742bd98f511601165e1cf0c16c683a199070df
Oliwier Kossak <s22018@pjwstk.edu.pl>
2023-05-07 10:07:24 +0200

    program which delete space in text

---
+++ char/delete_space_in_text.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+
+    char c;
+    
+    while((c=getchar())!=EOF){
+        if(isblank(c)){
+           //nothing
+        }
+        else{
+            putchar(c);
+        }
+
+    }
+
+    
+
+    return 0 ;
+}
